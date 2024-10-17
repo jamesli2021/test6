f06336cdddbc8640d142972ed0544a2a05b1462a
jamesli2021 <95068031+jamesli2021@users.noreply.github.com>
2024-10-17 12:21:35 +0800

    Add files via upload

---
+++ dist/_worker.js/middleware.mjs
@@ -0,0 +1,4 @@
+globalThis.process ??= {}; globalThis.process.env ??= {};
+import './chunks/astro-designed-error-pages_C8CakLQY.mjs';
+import './chunks/index_CRQO_XP4.mjs';
+export { onRequest } from './_astro-internal_middleware.mjs';
